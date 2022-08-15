6a72feaae276327f34ac15fc88bc10cd2adb40e8
Rauã <rauajosebr@gmail.com>
2022-08-15 03:52:16 -0300

    start

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function start(){
+  var del = document.getElementById("tela");
+  	if (tela.parentNode){
+  	 tela.parentNode.removeChild(tela);
+  	 var p = document.createElement("p");
+  	 document.body.appendChild(p);
+  	 p.innerHTML = "Hello World!";
+  	 p.setAttribute("class","start");
+  	}
+}
