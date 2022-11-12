53643f51ed596d353bfa64021bc334d30f07950a
Rauã <rauajosebr@gmail.com>
2022-11-12 02:19:57 -0300

    botoes

--- js/script.js
+++ js/script.js
@@ -2,12 +2,31 @@ function start(){
   var del = document.getElementById("tela");
   	if (tela.parentNode){
   	 tela.parentNode.removeChild(tela);
-  	 var p = document.createElement("p");
+  	var n = document.createElement("n"); 
+	var p = document.createElement("p");
   	 document.body.appendChild(p);
   	 p.innerHTML = "Hello World!";
-  	 p.setAttribute("class","start");
+	 p.setAttribute("class","start");
+
+	 document.body.appendChild(n);
+	 n.innerHTML = "Name: default";
+	 n.setAttribute("class","name");
   	 (function(){
 	 window.alert("inicio")}());
   	}
 
+}
+function avatar(){
+ var del = document.getElementById("tela");
+	if (tela.parentNode){
+	tela.parentNode.removeChild(tela);
+	var p = document.createElement("p");
+	var img = document.createElement("img");
+	p.innerHTML = "Name: default";
+	document.body.appendChild(p);
+	p.setAttribute("class","name")
+        document.body.appendChild(img);
+        img.setAttribute("src","img/robo_down_0.png");
+		img.setAttribute("class","img");
+	}
 }
